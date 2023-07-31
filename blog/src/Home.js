b3957c6abc4756efc181dc6560f65f728ef4f948
Rishik Chakravarty <rishikrc3@gmail.com>
2023-07-31 23:41:20 +0530

    demo_feed

--- blog/src/Home.js
+++ blog/src/Home.js
@@ -1,9 +1,13 @@
 import React from "react";
-
+import Feed from "./Feed";
 const Home = ({ posts }) => {
   return (
     <main className="Home">
-      <h1>Fok</h1>
+      {posts.length ? (
+        <Feed posts={posts} />
+      ) : (
+        <p style={{ marginTop: "2rem" }}>No posts to display.</p>
+      )}
     </main>
   );
 };
