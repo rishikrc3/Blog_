36b6408eb971187b921f6b9e950b738346bc227b
Rishik Chakravarty <rishikrc3@gmail.com>
2023-08-01 14:14:13 +0530

    fetch_data

--- blog/src/About.js
+++ blog/src/About.js
@@ -1,9 +1,11 @@
-import React from "react";
-
 const About = () => {
   return (
-    <main>
-      <h1>About</h1>
+    <main className="About">
+      <h2>About</h2>
+      <p style={{ marginTop: "1rem" }}>
+        This blog app is a project in the my journey of learning React and
+        Javascript.
+      </p>
     </main>
   );
 };
